85ae963b4755c7454f256336dae3b0dc9307f0bb
aleksandarskrbic <skrbic.alexa@gmail.com>
2020-07-22 17:07:58 +0200

    Dockerize web-ui and make run script

---
+++ ops/run-ui.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Build and run web-ui service
+cd ../web-ui/ && docker build -t web-ui . && docker run -it --rm -v ${PWD}:/app -v /app/node_modules -p 3000:3000 -e CHOKIDAR_USEPOLLING=true web-ui
+
