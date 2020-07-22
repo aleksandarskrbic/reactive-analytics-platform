169ebcd032cf2bbd62afc8d0d61245aacfc4cccc
aleksandarskrbic <skrbic.alexa@gmail.com>
2020-07-22 21:58:31 +0200

    format

--- query-service/src/main/java/com/github/aleksandarskrbic/http/exception/ExceptionHandler.java
+++ query-service/src/main/java/com/github/aleksandarskrbic/http/exception/ExceptionHandler.java
@@ -4,7 +4,6 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
-
 @Provider
 public class ExceptionHandler implements ExceptionMapper<EntityNotFound> {
 
